025770b3b36a9f4f4c1bd0c99d321774ece11f64
Asma Ali <128274795+AsmaAl12@users.noreply.github.com>
2023-07-29 00:24:13 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,46 @@
+//===========================JS CLASS 4==========================//
+
+
+//=====Arrays=====//
+//=====Array Methods=====//
+
+// const students = [
+//     "maha khan from peshawar",
+//     "bisma khan from peshawar",
+//     "ruhama khan from peshawar",
+//     "abdullah khan from peshawar",
+// ];
+
+// students.push("asma khan from peshawar")
+// students.pop()
+// students.unshift("asma khan from peshawar")
+// students.shift()
+// console.log(students);
+
+// add an elem in the last of array => push
+// delete an elem in the last of array => pop
+// delete an elem in the beginning of array => shift
+// add an elem in the beginning of array => unshift
+
+// for (let index = 0; index < students.length; index++) {
+// if (index === 2) {
+//     break
+// }
+// else {
+//     const eachStudent = index + 1 + " " + students[index]
+//     console.log(eachStudent);
+// }
+// console.log(index + 1);
+// console.log(students[index]);
+// if (index === 1) {
+// delete students[index]
+// delete students[index]
+// }
+// console.log(students[index]);
+// document.write(students[index] + "<br></br>");
+// }
+
+
+
+
+
